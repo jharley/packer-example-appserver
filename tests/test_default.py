6d08da3b459f7b03f656871a5426c70011b91b1b
Jason Harley <jharley@streetcontxt.com>
2017-11-07 19:46:53 -0500

    Initial taco app test

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,2 +1,8 @@
 def test_ami_launch(host):
     assert host.system_info.type == 'linux'
+
+def test_appserver(host):
+    assert host.socket('tcp://80').is_listening
+
+    cmd = host.run("curl -s http://localhost | grep 'Hello, DevOps TO'")
+    assert cmd.rc == 0
